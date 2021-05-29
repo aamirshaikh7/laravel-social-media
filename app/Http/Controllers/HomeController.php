9e9c64e1b799efe00096e9f1a71b7cd463b7a1bd
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-05-29 22:44:03 +0530

    Only displaying records which are created by the authenticated user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home', ['lweets' => Lweet::all()]);
+        return view('home',[
+            'lweets' => auth()->user()->timeline()
+        ]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Lweet;
 
 class User extends Authenticatable
 {
@@ -40,6 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function timeline () {
+        return Lweet::where('user_id', $this->id)->latest()->get();
+    }
 
     public function lweets () {
         return $this->hasMany(Lweet::class);
