b471eea4af0e6048a0efb9ae0f6f1d3116892471
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-05-30 23:13:37 +0530

    Storing new records

--- app/Models/Lweet.php
+++ app/Models/Lweet.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Lweet extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['body'];
     
     public function user () {
         return $this->belongsTo(User::class);
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::post('/lweets', [App\Http\Controllers\LweetController::class, 'store'])->name('lweets.store');
