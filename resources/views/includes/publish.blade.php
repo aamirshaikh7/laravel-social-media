0b8582c74fe486a6f12c8677cb1ab943ed787fb8
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-05-30 23:13:37 +0530

    Storing new record

--- resources/views/includes/publish.blade.php
+++ resources/views/includes/publish.blade.php
@@ -1,6 +1,11 @@
 <h1 class="display-5">What's on your mind?</h1>
-<form>
-    <textarea class="border rounded form-control mb-1" rows="6" maxlength="500"></textarea>
-    <img src="{{ auth()->user()->profile }}" alt="">
-    <button class="btn btn-secondary border rounded-pill float-end" type="button">Lweet it</button>
+<form method="POST" action="{{ route('lweets.store') }}">
+    @csrf
+
+    <textarea name="body" class="border rounded form-control mb-1" rows="6" maxlength="500"></textarea>
+    @error ('body')
+        <p class="text-danger">{{ $message }}</p>
+    @enderror
+    <img src="{{ auth()->user()->profile }}" alt="profile">
+    <button class="btn btn-secondary border rounded-pill float-end" type="submit">Lweet it</button>
 </form>
