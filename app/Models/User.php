7246ec18b93a8d6e47e9181fcf93232a10345fd8
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-05-31 20:16:23 +0530

    Following users

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,6 +54,10 @@ class User extends Authenticatable
         return $this->hasMany(Lweet::class);
     }
 
+    public function follow (User $user) {
+        return $this->follows()->save($user);
+    }
+    
     public function follows () {
         return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');
     }
