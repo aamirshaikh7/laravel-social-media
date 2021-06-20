f9f04005b5801c0788623fa9a14e9c0838725fa6
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-06-20 11:23:18 +0530

    Relweeting and undo relweet

--- app/Models/Relweetable.php
+++ app/Models/Relweetable.php
@@ -5,6 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 
 trait Relweetable {
+    public function relweet (User $user = null) {
+        $this->relweets()->updateOrCreate([
+            'user_id' => $user ? $user->id : auth()->id()
+        ], [
+            'is_relweeted' => true
+        ]);
+    }
+
+    public function undoRelweet (User $user = null) {
+        $this->relweets()->updateOrCreate([
+            'user_id' => $user ? $user->id : auth()->id()
+        ], [
+            'is_relweeted' => false
+        ]);
+    }
+
     public function relweets () {
         return $this->hasMany(Relweet::class);
     }
