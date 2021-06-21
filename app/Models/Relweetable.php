647e3445349292e880590e415aff818f24c46593
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-06-21 17:54:17 +0530

    Refactoring lweet and undo relweet methods

--- app/Models/Relweetable.php
+++ app/Models/Relweetable.php
@@ -5,20 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 
 trait Relweetable {
-    public function relweet (User $user = null) {
+    public function relweet (User $user = null, $relweet = true) {
         $this->relweets()->updateOrCreate([
             'user_id' => $user ? $user->id : auth()->id()
         ], [
-            'is_relweeted' => true
+            'is_relweeted' => $relweet
         ]);
     }
 
     public function undoRelweet (User $user = null) {
-        $this->relweets()->updateOrCreate([
-            'user_id' => $user ? $user->id : auth()->id()
-        ], [
-            'is_relweeted' => false
-        ]);
+        return $this->relweet($user, false);
     }
 
     public function relweets () {
