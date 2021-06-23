6c8a865b79976605aee105324d25723972dbf478
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-06-23 18:27:46 +0530

    Styling side-bar links

--- resources/views/includes/sidebar-links.blade.php
+++ resources/views/includes/sidebar-links.blade.php
@@ -1,9 +1,8 @@
 <div class="p-5 list-group">
-    <a class="list-group-item list-group-item-action" href="{{ route('home') }}">Home</a>
-    <a class="list-group-item list-group-item-action" href="#">Notifications</a>
-    <a class="list-group-item list-group-item-action" href="{{ route('explore.index') }}">Explore</a>
-    <a class="list-group-item list-group-item-action" href="{{ route('profiles.show', auth()->user()) }}">Profile</a>
-    <a class="list-group-item list-group-item-action" href="#">More</a>
+    <a class="list-group-item list-group-item-action {{ Request::path() === 'lweets' ? 'active' : '' }}" href="{{ route('home') }}">Home</a>
+    <a class="list-group-item list-group-item-action {{ Request::path() === 'notifications' ? 'active' : '' }}" href="#">Notifications</a>
+    <a class="list-group-item list-group-item-action {{ Request::path() === 'explore' ? 'active' : '' }}" href="{{ route('explore.index') }}">Explore</a>
+    <a class="list-group-item list-group-item-action {{ Request::path() === 'profiles/' . auth()->user()->username ? 'active' : '' }}" href="{{ route('profiles.show', auth()->user()) }}">Profile</a>
     <form method="POST" action="/logout">
         @csrf
 
